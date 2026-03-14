eb404eec6daea36537112e7a4aab449992c79e37
Swati Joshi <swatijoshi@outlook.de>
2026-03-14 16:14:26 +0100

    added use statement

--- src/public/index.php
+++ src/public/index.php
@@ -10,17 +10,20 @@ require_once __DIR__ . '/../app/classes/TransactionReader.php';
 require_once __DIR__ . '/../app/classes/TransactionCalculater.php';
 require_once __DIR__ . '/../app/helper.php';
 
+use app\classes\TransactionReader;
+use app\classes\TransactionCalculater;
+
 // Correct path to the directory
 $dirPath = __DIR__ . '/../transaction_files/';
 $transactions = [];
 $totals = [];
 
 // Read transactions
-$reader = new \app\classes\TransactionReader();
+$reader = new TransactionReader();
 $transactions = $reader->readDirectory($dirPath, 'transactionFormatter');
 
 // Calculate totals
-$calculateTotals = new \app\classes\TransactionCalculater();
+$calculateTotals = new TransactionCalculater();
 $totals = $calculateTotals->calculateTotal($transactions);
 
 require_once __DIR__ . '/../views/transactions-view.php';
